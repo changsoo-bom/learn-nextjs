b5174469560db5ec6045c21ca609b157d64e2c9c
김창수 <chang9811@interplug.co.kr>
2024-08-21 17:17:11 +0900

    Initial commit

---
+++ app/about-us/company/jobs/sales/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Sales(){
+    return(
+        <div>
+            <h1>Sales jobs!</h1>
+        </div>
+    )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { describe } from "node:test"
+import Navigation from "../components/navigation"
+import { title } from "node:process"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title:{
+    template: "%s | Next Movies",
+    default: "Next Movies",
+  },
+  description: 'The best movies on the best framework',
+}
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation/>
+        {children}
+      </body>
+    </html>
+  )
+}
